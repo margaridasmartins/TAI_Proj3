95421c9f6d0ae866c6d3789a0674f196505a3d09
Margarida <margarida.martins@ua.pt>
2022-01-13 11:11:07 +0000

    inital

---
+++ GetMaxFreqs/src/build.linux/CMakeFiles/GetMaxFreqs.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/media/guids/Hdd/TAI/GetMaxFreqs/bin/GetMaxFreqs"
+  "/media/guids/Hdd/TAI/GetMaxFreqs/bin/GetMaxFreqs.pdb"
+  "CMakeFiles/GetMaxFreqs.dir/GetMaxFreqs.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GetMaxFreqs.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
