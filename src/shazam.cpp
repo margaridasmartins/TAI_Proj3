c1ebafe3eace87c3eaeec683550d79c34470d312
DoctorTux <diogomendonca21@ua.pt>
2022-01-20 11:59:58 +0000

    compress first
    Co-authored-by: Margarida Martins <margaridasmartins@users.noreply.github.com>
    Co-authored-by: leand12 <leand12@users.noreply.github.com>

--- src/shazam.cpp
+++ src/shazam.cpp
@@ -1,6 +1,41 @@
-#include <shazam.hpp>
+#include "shazam.hpp"
 
-int main(int argc, char *argv[]) {
-    
+#include <fstream>
+#include <iostream>
+#include <boost/iostreams/filtering_streambuf.hpp>
+#include <boost/iostreams/copy.hpp>
+#include <boost/iostreams/filter/gzip.hpp>
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+
+    if (argc < 2)
+    {
+        cerr << "Usage: " << argv[0] << " <output .gz file>" << endl;
+    }
+
+    ifstream infile(argv[1], ios_base::in | ios_base::binary);
+
+    // char* str = "Hello";
+    // char dest[12];
+
+    // strcpy( dest, str );
+    // ;
+
+    // Read filename from the first command line argument
+    ofstream outfile(strcat(argv[1], ".gz"), ios_base::out | ios_base::binary);
+    boost::iostreams::filtering_streambuf<boost::iostreams::output> outbuf;
+    outbuf.push(boost::iostreams::gzip_compressor());
+    outbuf.push(infile);
+    // Convert streambuf to ostream
+    ostream out(&outbuf);
+    // Write some test data
+    out << "This is a test text!\n";
+    // Cleanup
+    boost::iostreams::close(outbuf); // Don't forget this!
+    outfile.close();
+    infile.close();
+
     return 0;
 }
--- src/shazam.hpp
+++ src/shazam.hpp
@@ -1,17 +1,33 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <filesystem>
+#include <iostream>
+#include <dirent.h>
+#include <string.h>
+#include <boost/iostreams/filter/gzip.hpp>
+#include <boost/iostreams/filter/bzip2.hpp>
 
+/*
 int compress_solo(FILE *fptr_db){
-
+    
 }
 
 int compress_cat(FILE *fptr_sample, FILE *fptr_db){
 
 }
 
-int normalized_compression_distance(FILE *fptr_sample, FILE *fptr_db, int solo_test){
-    //iterate through each sample
-        int solo_db = compress_solo(fptr_db);
-        int cat_db = compress_cat(fptr_sample,fptr_db);
-        //calculate distance according to formula
+
+int normalized_compression_distance(FILE *fptr_sample, DIR *path_dir, int solo_test){
+    char path[40]="../music_samples/";
+    struct dirent *entry;
+
+    if ((path_dir = opendir(path)) == NULL)
+        perror("opendir() error");
+    else {
+        while ((entry = readdir(path_dir)) != NULL)
+            int solo_db = compress_solo(strcat(path,entry->d_name));
+            int cat_db = compress_cat(fptr_sample,strcat(path,entry->d_name));
+        closedir(path_dir);
+    }
 }
+*/
