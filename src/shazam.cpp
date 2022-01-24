8f5524d9e3f36fd85cdc1a1c681dfc46dcc641ee
DoctorTux <diogomendonca21@ua.pt>
2022-01-24 19:47:26 +0000

    compression cmd
    Co-authored-by: leand12 <leand12@users.noreply.github.com>
    Co-authored-by: Margarida Martins <margaridasmartins@users.noreply.github.com>

--- src/shazam.cpp
+++ src/shazam.cpp
@@ -1,41 +1,49 @@
 #include "shazam.hpp"
 
-#include <fstream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <filesystem>
 #include <iostream>
-#include <boost/iostreams/filtering_streambuf.hpp>
-#include <boost/iostreams/copy.hpp>
-#include <boost/iostreams/filter/gzip.hpp>
+#include <fstream>
+#include <dirent.h>
+#include <string.h>
+
+
 using namespace std;
 
 int main(int argc, char *argv[])
 {
 
-    if (argc < 2)
-    {
-        cerr << "Usage: " << argv[0] << " <output .gz file>" << endl;
+    string help_text =
+      "Usage:\n"
+      "  ./shazam models_dir filename_t\n"
+      "Required:\n"
+      "  models_dir     The name of the directory with the language models"
+      "  filename_t       The name of the file with the text under analysis\n"
+      "Example:\n"
+      "  ./shazam ?? 2 0.5\n";
+
+    if (argc < 2) {
+        printf("ERR: Incorrect number of arguments\n\n%s", help_text.c_str());
+        exit(1);
     }
 
-    ifstream infile(argv[1], ios_base::in | ios_base::binary);
+    
+    char models_dir[100];
+    sprintf(models_dir, "../%s", argv[1]);
 
-    // char* str = "Hello";
-    // char dest[12];
+    char file_test[100];
+    sprintf(file_test, "../%s", argv[2]);
 
-    // strcpy( dest, str );
-    // ;
+    DIR *dp;
+    dp = opendir(models_dir);
 
-    // Read filename from the first command line argument
-    ofstream outfile(strcat(argv[1], ".gz"), ios_base::out | ios_base::binary);
-    boost::iostreams::filtering_streambuf<boost::iostreams::output> outbuf;
-    outbuf.push(boost::iostreams::gzip_compressor());
-    outbuf.push(infile);
-    // Convert streambuf to ostream
-    ostream out(&outbuf);
-    // Write some test data
-    out << "This is a test text!\n";
-    // Cleanup
-    boost::iostreams::close(outbuf); // Don't forget this!
-    outfile.close();
-    infile.close();
+    if (dp == NULL) {
+        perror("opendir: Path does not exist or could not be read.");
+        return -1;
+    }
 
+    normalized_compression_distance(file_test, dp);
+    
     return 0;
 }
--- src/shazam.hpp
+++ src/shazam.hpp
@@ -2,32 +2,66 @@
 #include <stdio.h>
 #include <filesystem>
 #include <iostream>
+#include <fstream>
 #include <dirent.h>
 #include <string.h>
-#include <boost/iostreams/filter/gzip.hpp>
-#include <boost/iostreams/filter/bzip2.hpp>
 
-/*
-int compress_solo(FILE *fptr_db){
-    
+int gzip_compress(char *filename){
+    char command[30]="gzip -k -9 ";
+    printf("%s\n",filename);
+    system(strcat(command, filename));
+    char final_filename[50];
+    strcpy(final_filename, filename);
+    FILE *fp = fopen(strcat(final_filename, ".gz"), "rb");
+    fseek(fp,0,SEEK_END);
+    return ftell(fp);
 }
 
-int compress_cat(FILE *fptr_sample, FILE *fptr_db){
+int lzma_compress(char *filename){
+    char command[30]="lzma -k -9 ";
+    printf("%s\n",filename);
+    system(strcat(command, filename));
+    char final_filename[50];
+    strcpy(final_filename, filename);
+    FILE *fp = fopen(strcat(final_filename, ".lzma"), "rb");
+    fseek(fp,0,SEEK_END);
+    return ftell(fp);
+}
 
+int bzip2_compress(char *filename){
+    char command[30]="bzip2 -k -9 ";
+    printf("%s\n",filename);
+    system(strcat(command, filename));
+    char final_filename[50];
+    strcpy(final_filename, filename);
+    FILE *fp = fopen(strcat(final_filename, ".bz2"), "rb");
+    fseek(fp,0,SEEK_END);
+    return ftell(fp);
+}
+
+int compress_solo(char *filename){
+    gzip_compress(filename);
+    lzma_compress(filename);
+    bzip2_compress(filename);
+    return 1;
+}
+
+int compress_cat(char *filename, char *file_db){
+    return 0;
 }
 
 
-int normalized_compression_distance(FILE *fptr_sample, DIR *path_dir, int solo_test){
+int normalized_compression_distance(char *filename, DIR *path_dir){
     char path[40]="../music_samples/";
     struct dirent *entry;
 
     if ((path_dir = opendir(path)) == NULL)
         perror("opendir() error");
     else {
-        while ((entry = readdir(path_dir)) != NULL)
+        while ((entry = readdir(path_dir)) != NULL){
             int solo_db = compress_solo(strcat(path,entry->d_name));
-            int cat_db = compress_cat(fptr_sample,strcat(path,entry->d_name));
+            int cat_db = compress_cat(filename,strcat(path,entry->d_name));
+        }
         closedir(path_dir);
     }
 }
-*/
